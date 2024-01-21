fd9b7dacfcaa5131c017d2b109738d74d25fc91f
juanignaciogiacobbe <jgiacobbe@fiuba.ar>
2024-01-21 20:28:57 -0300

    añado ejercicio del martes tecnico #1

--- martes_tecnico_1/main.py
+++ martes_tecnico_1/main.py
@@ -1,1 +1,16 @@
-print('Hello world')
+# Construir un programa que cuente la cantidad de palabras de una frase. 
+# Por ejemplo, si se le ingresa la frase "desarrollar software es lo mejor que me pasó",
+# la salida del programa debería ser 8.
+
+def contadorPalabras(frase):
+    fraseArr = frase.split(" ")
+    longitud = len(fraseArr)
+    return longitud
+
+def testContadorPalabras():
+    assert contadorPalabras("desarrollar software es lo mejor que me pasó") == 8, "Deberia ser 8"
+    assert contadorPalabras("messi capo") == 2, "Deberia ser 2"
+    print("todo salio bien")
+    
+
+testContadorPalabras()
